4341f3783e2b3a1b65a8e8e85c515aafd1d64bd9
Anon1234 <forspam@mail.ua>
2012-11-04 16:59:38 +0200

    fsdfsg

--- Tuz_Skript.meta.js
+++ Tuz_Skript.meta.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name        Tuz Skript
-// @namespace   std
+// @namespace   https://github.com/Anon1234/
 // @include     http://tuzach.in/
 // @include     http://tuzach.in/#*
 // @grant       none
 // @version     2.6
 // @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.meta.js
+// @downloadURL https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.user.js
 // ==/UserScript==
