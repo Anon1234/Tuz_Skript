0614665a18545e80f26120b39feb9c1804be03f5
Anon1234 <forspam@mail.ua>
2012-11-04 16:58:25 +0200

    fix namespace

--- Tuz_Skript.user.js
+++ Tuz_Skript.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name        Tuz Skript
-// @namespace   std
+// @namespace   https://github.com/Anon1234/
 // @include     http://tuzach.in/
 // @include     http://tuzach.in/#*
 // @grant       none
 // @version     2.6
-// @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.user.js
+// @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.meta.js
+// @downloadURL https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.user.js
 // ==/UserScript==
 
 var css_style = document.createElement('style')
