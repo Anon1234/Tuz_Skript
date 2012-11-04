cd9f66497803550c341e65ac82f57678faff83dd
Anon1234 <forspam@mail.ua>
2012-11-04 17:28:34 +0200

    еще тест

--- Tuz_Skript.user.js
+++ Tuz_Skript.user.js
@@ -4,7 +4,7 @@
 // @include     http://tuzach.in/
 // @include     http://tuzach.in/#*
 // @grant       none
-// @version     2.7
+// @version     2.7.1
 // @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.meta.js
 // @downloadURL https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.user.js
 // ==/UserScript==
