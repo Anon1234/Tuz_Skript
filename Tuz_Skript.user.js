22dca7092ee4c88dd27b160404137fbd8081c68f
Anon1234 <forspam@mail.ua>
2012-11-06 20:37:02 +0200

    12345

--- Tuz_Skript.user.js
+++ Tuz_Skript.user.js
@@ -4,9 +4,8 @@
 // @include     http://tuzach.in/
 // @include     http://tuzach.in/#*
 // @grant       none
-// @version     2.8.2
+// @version     2.8.3
 // @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.user.js
-// @downloadURL https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.user.js
 // ==/UserScript==
 
 var css_style = document.createElement('style')
