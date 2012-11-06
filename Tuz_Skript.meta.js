34c69e1ba62b32b4488f47d104b47ebb22ed04de
Anon1234 <forspam@mail.ua>
2012-11-06 20:43:03 +0200

    removed meta.js

--- Tuz_Skript.meta.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name        Tuz Skript
-// @namespace   https://github.com/Anon1234/
-// @include     http://tuzach.in/
-// @include     http://tuzach.in/#*
-// @grant       none
-// @version     2.8.1
-// @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.meta.js
-// ==/UserScript==
