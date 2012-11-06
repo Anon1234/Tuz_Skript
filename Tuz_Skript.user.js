1885fbcc54d12298999c1d83edc4b23ac060e1e0
Anon1234 <forspam@mail.ua>
2012-11-06 20:28:40 +0200

    Фикс автообновления для хрома.

--- Tuz_Skript.user.js
+++ Tuz_Skript.user.js
@@ -4,8 +4,8 @@
 // @include     http://tuzach.in/
 // @include     http://tuzach.in/#*
 // @grant       none
-// @version     2.8.1
-// @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.meta.js
+// @version     2.8.2
+// @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.user.js
 // ==/UserScript==
 
 var css_style = document.createElement('style')
