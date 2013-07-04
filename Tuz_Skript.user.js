85aa7c55fa9aa5effd9944434c25223f108ecd69
Anon1234 <forspam@mail.ua>
2013-07-04 12:13:27 +0300

    Новая нумерация версий в формате Год.Месяц.Число.Патч

--- Tuz_Skript.user.js
+++ Tuz_Skript.user.js
@@ -4,7 +4,7 @@
 // @include     http://tuzach.in/
 // @include     http://tuzach.in/#*
 // @grant       none
-// @version     2.9.8
+// @version     13.7.4.0
 // @updateURL   https://github.com/Anon1234/Tuz_Skript/raw/master/Tuz_Skript.user.js
 // @icon        https://github.com/Anon1234/Tuz_Skript/raw/master/blue_tuz.png
 // ==/UserScript==
